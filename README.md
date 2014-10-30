5558e3db81342de6fb81a4e0392abd5d2c55b3d3
startxfr <clarue@startx.fr>
2014-10-31 00:23:00 +0100

    modif du script d'install

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 sxcmd
 =====
 
-startx command
+
+
+
+
+##install 
+
+To install this program you can use the following command. It will download and start the install script who will handle to download and register the startx command line tool.
+
+```
+[user@localhost]$ : $(< curl http://www.github.com/startxfr/sxcmd/install.sh);
+```
+
+This script require that you have a valid home directory and write access to it ;)
--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 # Script use to save the database collections
 
 homedir=~
+origin=`pwd`
 userdir=.sxcmd
 ep="[sxcmd-install]"
-sourceurl="https://api.github.com/startxfr/sxcmd/download"
-sourcefile="master.tar.gz"
+sourceurl="https://github.com/startxfr/sxcmd/archive/"
+sourcefile="master.zip"
 
 echo $ep ""
 echo $ep "  +-----------------+"
@@ -20,60 +21,31 @@ fi;
 
 cd $homedir/$userdir
 echo $ep "downloading source file "
-curl -LSs $sourceurl/$sourcefile &> /dev/null
-if [ ! -d $homedir/$userdir/$sourcefile ]; then
+curl $sourceurl/$sourcefile > $homedir/$userdir/$sourcefile
+if [ -e $homedir/$userdir/$sourcefile ]; then
     echo $ep "extracting program "
-    tar xzvf $sourcefile &> /dev/null
-    echo $ep "cleanup installation "
-    rm -f install.sh .gitignore
-    cd - &> /dev/null
-    rm -f install.sh
-    echo $ep "register program"
-    echo "" >> $homedir/.bashrc
-    echo "export PATH=\$PATH:$homedir/$userdir/bin"  >> $homedir/.bashrc
-    . $homedir/.bashrc &> /dev/null
-    echo $ep "SUCCESS : Installation is completed and you can start to use the 'sxcmd' from the commande line"
-    exit;
+    unzip $sourcefile &> /dev/null
+    if [ -d $homedir/$userdir/sxcmd-master ]; then
+        cd sxcmd-master
+        echo $ep "cleanup installation "
+        rm -f install.sh .gitignore
+        mv * ../
+        cd -
+        rm -f $sourcefile
+        cd $origin &> /dev/null
+        rm -f install.sh
+        echo $ep "register program"
+        echo "" >> $homedir/.bashrc
+        echo "export PATH=\$PATH:$homedir/$userdir/bin"  >> $homedir/.bashrc
+        . ~/.bashrc
+        echo $ep "SUCCESS : Installation is completed and you can start to use the 'sxcmd' from the commande line (if it doesn't work, start a new terminal !)"
+        exit;
+    else
+        rm -f $homedir/$userdir/$sourcefile
+        echo $ep "ERROR : Could not find sxcmd-master in source code"
+        exit;
+    fi;
 else
     echo $ep "FATAL : Could not download sources from server"
     exit;
 fi;
-
-
-
-
-echo "print('_ ' + db.getCollectionNames())" > $tmp_file
-cols=`mongo $db $tmp_file | grep '_' | awk '{print $2}' | tr ',' ' '`
-rm $tmp_file
-echo_prefix="[pre-commit hook]"
-
-# execute doc-generate and comit-push to github
-function dumpDatabases {
-   echo -e "$echo_prefix Start dumping database"
-   for c in $cols
-    do
-        if [[ $c != "logs" ]] && [[ $c != "startx.logs" ]] && [[ $c != "sxapi.session" ]] ; then
-        echo -e "$echo_prefix Exporting $c "
-        mongoexport -d $db -c $c -o "$out_dir/dump_${db}_${c}.json" --jsonArray &> /dev/null
-        fi
-   done
-   git add $out_dir/*
-   echo -e "$echo_prefix End dumping database"
-}
-
-
-echo -e "$echo_prefix Start pre-commit process"
-# Allows us to read user input below, assigns stdin to keyboard
-exec < /dev/tty
-while true; do
-    read -p "$echo_prefix Do you wan't to dump database(s) ? (Y/n) " yn
-    if [ "$yn" = "" ]; then
-       yn='Y'
-    fi
-    case $yn in
-       [Yy] ) dumpDatabases; exit 0;;
-       [Nn] ) exit 0;;
-       * ) echo -e "$echo_prefix Please answer y or n for yes or no.";;
-    esac
-done
-echo -e "$echo_prefix End pre-commit process"
